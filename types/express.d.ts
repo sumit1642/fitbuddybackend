8420d8fda39358f1e8ff6eba6705d7923179df1a
Sumit1642 <sumeetyadav1642@gmail.com>
2025-12-25 15:44:47 +0530

    feat: Solved type declarations for Express Request interface

---
+++ types/express.d.ts
@@ -0,0 +1,14 @@
+// types/express.d.ts
+
+import { Request } from "express";
+
+declare global {
+	namespace Express {
+		interface Request {
+			user: {
+				id: string;
+				email: string;
+			};
+		}
+	}
+}
