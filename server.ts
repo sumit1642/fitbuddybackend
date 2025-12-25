c7bb41e95655e9e1999e1e4a7103bd295245c9f9
Sumit1642 <sumeetyadav1642@gmail.com>
2025-12-25 19:41:10 +0530

    feat: refactor server initialization to use HTTP server with Socket.IO

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
+import http from "http";
 import express from "express";
 import { env } from "./config/env.js";
 import routes from "./routes/index.js";
 import { errorHandler } from "./middleware/error.middleware.js";
-import "./repositories/db.js";
 import { initRedis } from "./infrastructure/redis/client.js";
+import { initSocketServer } from "./realtime/socket.server.js";
+import "./repositories/db.js";
 
 const app = express();
 
@@ -15,9 +17,14 @@ app.use("/api", routes);
 // error handler (last)
 app.use(errorHandler);
 
+// Initialize Redis
 await initRedis();
 
+// Create HTTP server and attach Socket.IO
+const server = http.createServer(app);
+initSocketServer(server);
 
-app.listen(env.PORT, () => {
+// Start server
+server.listen(env.PORT, () => {
 	console.log(`🚀 Server running on http://localhost:${env.PORT}`);
 });
